2474e4aec47f47b7b38d79de45edf19764b6f0e7
Иван Мочалов <mochalov.ie.kst@ad.local>
2023-10-16 16:39:06 +0300

    correct code

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -11,7 +11,7 @@ import { instanceOfTBoard } from '../../utils/instanceOfTBoard';
 import { instanceOfTTask } from '../../utils/instanceOfTTask';
 
 export const Layout = () => {
-  const [initialValue, setInitialValue] = useLocalStorage<TInitialData | Object>({ steps: {}, tasks: {}, boards: {}, boardOrder: [] }, 'boardsList')
+  const [initialValue, setInitialValue] = useLocalStorage<TInitialData | Object>({ steps: {}, tasks: {}, boards: {}, boardOrder: [] }, 'boardsList');
 
   const { board_id } = useParams<{ board_id: string }>();
   const currentBoard = initialValue.boards && initialValue.boards[board_id || ''];
